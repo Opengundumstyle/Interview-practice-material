e5cc9cb6b3cd9c536a9e8de86db4a9314adeee2c
Opengundumstyle <zhlin04@gmail.com>
2021-11-06 17:48:49 -0700

    refactored , easy

--- LeetCode/RomanInt.js
+++ LeetCode/RomanInt.js
@@ -68,3 +68,32 @@ console.log(romanToInt('IX'))
 console.log(romanToInt('LVIII'))
 console.log(romanToInt('MCMXCIV'))
 
+// ** alt cleaner version ** 
+
+const chars = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+
+var romanToInt = function(s) {
+  let output = 0;
+  let last = 0;
+
+  for (let i = s.length - 1; i > -1; i--) {
+    const char = s.charAt(i);
+    const n = chars[char];
+
+    if (last && last > n) {
+      output -= n;
+    } else {
+      output += n;
+    }
+
+    last = n;
+  }
+
+  return output;
+}
+
+console.log(romanToInt('III'))
+console.log(romanToInt('IV'))
+console.log(romanToInt('IX'))
+console.log(romanToInt('LVIII'))
+console.log(romanToInt('MCMXCIV'))
