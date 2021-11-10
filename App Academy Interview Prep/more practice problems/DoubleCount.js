3a08e59172d27daaf69eee94763d25fbefb6d4b0
Opengundumstyle <zhlin04@gmail.com>
2021-11-09 22:35:43 -0800

    practice

---
+++ App Academy Interview Prep/more practice problems/DoubleCount.js
@@ -0,0 +1,17 @@
+/*Double Letter Count
+Write a function doubleLetterCount that takes in a string and returns the number of 
+times that the same letter repeats twice in a row. */
+
+function doubleLetterCount(string) {
+    let count = 0;
+   for(let i = 0 ; i < string.length; i ++){
+       
+      if(string[i] === string[i+1] && string[i] !== string[i -1]){
+          count ++
+      }
+   }
+   return count; 
+}
+
+console.log(doubleLetterCount("the jeep rolled down the hill"));  // 3
+console.log(doubleLetterCount("bootcamp")); // 1
