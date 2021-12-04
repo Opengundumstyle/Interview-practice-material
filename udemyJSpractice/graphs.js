7039da9befde4f08247026438aa74976b222aa7f
Opengundumstyle <zhlin04@gmail.com>
2021-12-03 16:13:03 -0800

    added dfsIterative

--- udemyJSpractice/graphs.js
+++ udemyJSpractice/graphs.js
@@ -20,5 +20,66 @@ class Graph{
        }
        delete this.adjacencyList[vertex]
    }
-   
+   DFSrecursive(start){
+       const result = [];
+       const visited = {};
+       const adjacencyList = this.adjacencyList
+
+       (function dfs(vertex) {
+          if(!vertex) return null;
+          visited[vertex] = true;
+          result.push(vertex);
+          adjacencyList[vertex].forEach(neighbor => {
+              if(!visited[neighbor]){
+                  return dfs(neighbor)
+              }
+          }) 
+       })(start)
+       return result;
+   }
+
+   DFSiterative(start){
+     const stack = [start];
+     const result = [];
+     const visited = {};
+     let currrentVertex
+     visited[start] = true;
+     while(stack.length){
+         currrentVertex = stack.pop();
+         result.push(currrentVertex);
+
+         this.adjacencyList[currrentVertex].forEach(neighbor => {
+             if(!visited[neighbor]){
+                 visited[neighbor] = true;
+                 stack.push(neighbor)
+             }
+         }); 
+     }
+     return result;
+   }
 }
+
+let g = new Graph();
+
+g.addVertex('A');
+g.addVertex('B');
+g.addVertex('C');
+g.addVertex('D');
+g.addVertex('E');
+g.addVertex('F');
+
+g.addEdge('A','B');
+g.addEdge('A','C');
+g.addEdge('B','D');
+g.addEdge('C','E');
+g.addEdge('D','E');
+g.addEdge('D','F');
+g.addEdge('E','F');
+
+//       A
+//    /    \
+//    B    C
+//    |    |
+//    D ---E
+//    \    /
+//       F
