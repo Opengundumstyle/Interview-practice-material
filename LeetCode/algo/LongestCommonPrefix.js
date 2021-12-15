b99d55b0a937fe9e3be769d3474373665689224d
Opengundumstyle <zhlin04@gmail.com>
2021-12-14 22:37:51 -0800

    easy

--- LeetCode/algo/LongestCommonPrefix.js
+++ LeetCode/algo/LongestCommonPrefix.js
@@ -17,26 +17,27 @@ Explanation: There is no common prefix among the input strings.
 
 */
 /**
- * @param {string[]} strs
- * @return {string}
- */
- var longestCommonPrefix = function(strs) {
+* @param {string[]} strs
+* @return {string}
+*/
+var longestCommonPrefix = function(strs) {
     let longest = '';
-    if(strs.length === 0){
-        return longest;
-    }
-    let comparisonWord = strs[0];
-    let comparisonIdx = 0;
-    for(let comparisonLetter of comparisonWord){
-        for(let i = 0; i < strs.length;i++){
-            let currentWord = strs[i];
-            let currentletter = currentWord.charAt(comparisonIdx);
+   if(strs.length === 0){
+       return longest;
+   }
+   let comparisonWord = strs[0];
+   let comparisonIdx = 0;
+   for(let comparisonLetter of comparisonWord){
+       for(let i = 0; i < strs.length;i++){
+           let currentWord = strs[i];
+           let currentletter = currentWord.charAt(comparisonIdx);
 
-            if(comparisonLetter !== currentletter || comparisonIdx >currentWord.length) {
-                return longest;
-            }
-        }
-        comparisonIdx;
-        longest += comparisonLetter;
-    }
+           if(comparisonLetter !== currentletter || comparisonIdx >currentWord.length) {
+               return longest;
+           }
+       }
+       comparisonIdx++;
+       longest += comparisonLetter;
+   }
+   return longest
 };
