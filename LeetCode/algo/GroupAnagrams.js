3a7da33492f119286e0e96fa8f40533a0669e4eb
Opengundumstyle <zhlin04@gmail.com>
2022-04-12 02:03:39 -0700

    solved

--- LeetCode/algo/GroupAnagrams.js
+++ LeetCode/algo/GroupAnagrams.js
@@ -26,7 +26,7 @@
  */
  var groupAnagrams = function(strs) {
     let cache = {};
-    const primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97];
+    const primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101];
     for(let str of strs){
          let Key = str.split('').reduce((total,char)=>total * primes[char.charCodeAt()-97],1);
          (!cache[Key])? cache[Key] = [str] : cache[Key].push(str);
